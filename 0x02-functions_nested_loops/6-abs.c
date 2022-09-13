ace45449f4a87d1dd77e15bfe02d7d0a55635e71
Solomon Peter Ehigiamusoe <ehigiamusoepeter93@gmail.com>
2022-09-13 06:10:56 -0700

    Task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,22 +1,16 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
+ * _abs - main function
  *
- * Return: Always 0.
+ * @n: integer to make absolute
+ * Return: int or int times negative
+ *
  */
-int main(void)
-{
-    int r;
 
-    r = _abs(-1);
-    printf("%d\n", r);
-    r = _abs(0);
-    printf("%d\n", r);
-    r = _abs(1);
-    printf("%d\n", r);
-    r = _abs(-98);
-    printf("%d\n", r);
-    return (0);
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (n * -1);
 }
